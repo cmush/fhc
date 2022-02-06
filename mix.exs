5dbf283f1d4f76a88c1d8d4c5e88dea69330302a
cmush <collinsmucheru@gmail.com>
2022-02-06 09:29:02 +0300

    add deps

--- mix.exs
+++ mix.exs
@@ -24,6 +24,9 @@ defmodule Fhc.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:finch, "~> 0.10.2"},
+      {:multipart, "~> 0.2.0"},
+      {:jason, "~> 1.3"}
     ]
   end
 end
