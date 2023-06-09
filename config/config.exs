3919624d9d245a7bb7d354c7db27c4b93585ff45
cmush <collinsmucheru@gmail.com>
2023-06-09 18:56:29 +0300

    allow configuring logs

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,4 @@ config :fhc,
   base_url: System.get_env("BASE_URL") || "http://localhost:4000",
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "25")
 
-config :logger, level: String.to_existing_atom(System.get_env("LOG_LEVEL") || "error")
+config :logger, level: String.to_existing_atom(System.get_env("FHC_LOG_LEVEL") || "info")
