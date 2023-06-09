f2db26a0db9ed674192feb1e9796ba9adb5b3b36
cmush <collinsmucheru@gmail.com>
2023-06-09 17:30:35 +0300

    allow configuring logs

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,5 @@ import Config
 config :fhc,
   base_url: System.get_env("BASE_URL") || "http://localhost:4000",
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "25")
+
+config :logger, level: String.to_existing_atom(System.get_env("LOG_LEVEL") || "error")
